84fb92626f0144db5fb65efa7e8142bda9deb999
Valerii Gusev <valerii.gusev@cognite.com>
2021-01-18 09:11:57 +0100

    Add more stories

--- stories/EmailsInput.stories.js
+++ stories/EmailsInput.stories.js
@@ -10,12 +10,44 @@ const EmailsEditorTemplate = ({ ...args }) => {
   return emailsInput.getContainer();
 };
 
+const MultiEmailsEditorTemplate = ({ ...args }) => {
+  const emailsInput1 = new EmailsInput({ ...args });
+  const emailsInput2 = new EmailsInput({ ...args });
+  const emailsInput3 = new EmailsInput({ ...args });
+
+  const container = document.createElement('div');
+  container.append(emailsInput1.getContainer());
+  container.append(emailsInput2.getContainer());
+  container.append(emailsInput3.getContainer());
+
+  return container;
+};
+
 const FormTemplate = ({ ...args }) => {
   return createForm({ ...args });
 };
 
-export const EmailsEditor = EmailsEditorTemplate.bind({});
-EmailsEditor.args = {};
+const MultiFormTemplate = ({ ...args }) => {
+  const container = document.createElement('div');
+  const form1 = createForm({ ...args });
+  const form2 = createForm({ ...args });
+  const form3 = createForm({ ...args });
+  container.append(form1);
+  container.append(form2);
+  container.append(form3);
+
+  return container;
+};
 
 export const Form = FormTemplate.bind({});
 Form.args = {};
+
+export const EmailsEditor = EmailsEditorTemplate.bind({});
+EmailsEditor.args = {};
+
+export const MultiEmailsEditor = MultiEmailsEditorTemplate.bind({});
+MultiEmailsEditor.args = {};
+
+export const MultiForm = MultiFormTemplate.bind({});
+MultiForm.args = {};
+
